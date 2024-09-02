19da53d0ac64b2a1682dc1e9862f0d3f883a8e4e
thinkpad_ubuntu <zeras4z4@gmail.com>
2024-09-02 22:10:54 +0100

    in progress

--- scripts/scrape_test.py
+++ scripts/scrape_test.py
@@ -4,7 +4,8 @@ from wiki import BeautifulSoup as bs, fetchPage
 
 # 1) extract links of categories
 #     1. extract links of books
-#     2. add books to the dataframe with the respective category
+#     2. extract number of pages for this category
+#       1. add books to the dataframe with the respective category
 
 url = "https://books.toscrape.com/"
 def scrape_books():
@@ -16,11 +17,35 @@ def scrape_books():
         soup = bs(response.content, features="html.parser")
         logging.info("Created the soup.")
 
-        categories = soup.find_all()
+        categories = soup.find(class_="nav nav-list").find('ul')
 
+        
+        for category in categories:
+            a_tag = category.find('a')
+
+            if type(a_tag) is not int: # for some reason some of the results are -1
+                category_link = a_tag.get('href')
+                category_name = category
+        
+                category_page = bs(fetchPage(url+category_link).content, features="html.parser")
+
+                num_pages = 1
+                num_pages_tag = category_page.find(class_="current")
+                if(num_pages_tag is not None):
+                    print(num_pages_tag.text.split("of ",1)[1])
+
+                    num_pages = int(num_pages_tag.text.split("of ",1)[1])
+                    
+                    
+                for i in range(num_pages):
+                    # page n is just page-n.html instead of index.html
+                    pritn('stub')
+            
        
-        return books
+        #return books
     except Exception as e:
         logging.error(f"Error scraping Oscar-winning films: {str(e)}")
         raise
 
+
+scrape_books()
