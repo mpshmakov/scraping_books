b9a83601f228f669961ddecd3c1e682f7d3a3311
mpshmakov <zeras4z4@gmail.com>
2024-09-06 05:42:14 +0100

    added script option which uses multithreading because the regular script was too slow

--- scripts/scrape_test_multithread.py
+++ scripts/scrape_test_multithread.py
@@ -1,3 +1,26 @@
+import concurrent.futures
+import time
+
+# def worker(number):
+#     for i in range (100):
+#         time.sleep(0.1)
+#         print(number, i)
+
+# import threading
+
+# print(threading.active_count())
+# print(threading.enumerate())
+
+# t1 = threading.Thread(target=worker, name='t1', args=(1,))
+# t2 = threading.Thread(target=worker, name='t2', args=(2,))
+
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+
 # figuring out beautiful soup
 import logging
 import uuid
@@ -42,6 +65,64 @@ from sbooks import fetchPage
 #           1. add book to the dataframe with the respective category
 
 url = "https://books.toscrape.com/"
+
+def category_worker(category):
+    books = []
+
+    a_tag = category.find('a')
+
+    if type(a_tag) is not int: # for some reason some of the results are -1
+        category_url = a_tag.get('href')
+        category_name = a_tag.string.strip()
+        print("category name: ",category_name)
+
+        category_page = bs(fetchPage(url+category_url).content, features="html.parser")
+
+        num_pages = 1
+        num_pages_tag = category_page.find(class_="current")
+
+        if(num_pages_tag is not None):
+            print("num_pages is not None")
+            num_pages = int(num_pages_tag.text.split("of ",1)[1])
+        print("pages: ", num_pages)
+
+        new_page_url = category_url
+        for i in range(num_pages):
+            iterator = i+1
+
+            # page n is just page-n.html instead of index.html
+            if (iterator != 1):
+                new_page_url = category_url.replace("index", "page-"+str(iterator))
+            print("current url: ", url+new_page_url)
+            current_page = bs(fetchPage(url+new_page_url).content, features="html.parser")
+
+            # get links of books
+            books_tag = current_page.find('ol')
+            books_a_tags = books_tag.find_all('a', title=True)
+            
+            for book_a in books_a_tags:
+                book_url = book_a.get('href').split("/", 3)[3]
+                print("book_url", book_url)
+
+                # soup of the book -> parse the details into a dict 
+                print("\nfound: ")
+
+                book_page = bs(fetchPage(url+"catalogue/"+book_url).content, features="html.parser")
+                main_div_tag = book_page.find(class_="col-sm-6 product_main")
+
+                id = str(uuid.uuid4())
+                title = get_title(main_div_tag)
+                price = get_price(main_div_tag)
+                availability = get_availability(main_div_tag)
+                rating = get_rating(main_div_tag)
+                the_category = category_name
+                books.append([id, title, price, availability, rating, the_category])
+                print("\n")
+    else:
+        return
+
+    print("total books", len(books))
+    return books
 
 def word_number_to_int(number):
     if number == "One":
@@ -93,56 +174,19 @@ def scrape_books():
 
         categories = soup.find(class_="nav nav-list").find('ul')
 
+        categories_list = []
+
         for category in categories:
-            a_tag = category.find('a')
+            categories_list.append(category)
 
-            if type(a_tag) is not int: # for some reason some of the results are -1
-                category_url = a_tag.get('href')
-                category_name = a_tag.string.strip()
-                print("category name: ",category_name)
-
-                category_page = bs(fetchPage(url+category_url).content, features="html.parser")
-
-                num_pages = 1
-                num_pages_tag = category_page.find(class_="current")
-
-                if(num_pages_tag is not None):
-                    print("num_pages is not None")
-                    num_pages = int(num_pages_tag.text.split("of ",1)[1])
-                print("pages: ", num_pages)
-
-                new_page_url = category_url
-                for i in range(num_pages):
-                    iterator = i+1
-
-                    # page n is just page-n.html instead of index.html
-                    if (iterator != 1):
-                        new_page_url = category_url.replace("index", "page-"+str(iterator))
-                    print("current url: ", url+new_page_url)
-                    current_page = bs(fetchPage(url+new_page_url).content, features="html.parser")
-
-                    # get links of books
-                    books_tag = current_page.find('ol')
-                    books_a_tags = books_tag.find_all('a', title=True)
-                    
-                    for book_a in books_a_tags:
-                        book_url = book_a.get('href').split("/", 3)[3]
-                        print("book_url", book_url)
-
-                        # soup of the book -> parse the details into a dict 
-                        print("\nfound: ")
-
-                        book_page = bs(fetchPage(url+"catalogue/"+book_url).content, features="html.parser")
-                        main_div_tag = book_page.find(class_="col-sm-6 product_main")
-
-                        id = str(uuid.uuid4())
-                        title = get_title(main_div_tag)
-                        price = get_price(main_div_tag)
-                        availability = get_availability(main_div_tag)
-                        rating = get_rating(main_div_tag)
-                        the_category = category_name
-                        books.append([id, title, price, availability, rating, the_category])
-                        print("\n")
+        with concurrent.futures.ThreadPoolExecutor(max_workers=40) as executor:
+            books_map = executor.map(category_worker, categories_list)
+        
+        for book in books_map:
+            if book != None:
+                print(book)
+                for j in range(len(book)):
+                    books.append(book[j])
 
         return books
     except Exception as e:
@@ -151,4 +195,4 @@ def scrape_books():
 
 
 b = scrape_books()
-print(b)
+print(len(b))
